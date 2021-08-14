13ada5b9a54f814bfe216253be1638449a347ce2
dinakarsvin@gmail.com <dinakarsvin@gmail.com>
2021-08-14 18:48:17 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+df = spark.read.format("csv") \
+    .option("inferSchema","true") \
+    .option("header","true") \
+    .option("sep",",") \
+    .load("/FileStore/tables/ind.csv")
+
+display(df)
